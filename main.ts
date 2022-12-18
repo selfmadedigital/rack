4b70328d2826e972a9d4d6cc780daa11de4ed759
selfmadedigital <baluch.dominik@gmail.com>
2022-12-18 23:39:17 +0100

    Aktualizovať pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 input.onButtonPressed(Button.A, function () {
     speed1 += 1
     changeSpeed(1, speed1)
+    OLED.writeNumNewLine(speed1)
 })
 function changeSpeed (fan: number, speed: number) {
     speed = speed * 100
@@ -15,15 +16,20 @@ function changeSpeed (fan: number, speed: number) {
 input.onButtonPressed(Button.B, function () {
     speed2 += 1
     changeSpeed(2, speed2)
+    OLED.writeNumNewLine(speed2)
 })
 let speed = 0
 let speed2 = 0
 let speed1 = 0
+OLED.init(128, 64)
 bluetooth.startUartService()
 speed1 = 0
 speed2 = 0
 pins.digitalWritePin(DigitalPin.P0, 0)
 pins.digitalWritePin(DigitalPin.P1, 0)
+loops.everyInterval(60000, function () {
+	
+})
 basic.forever(function () {
     bluetooth.uartWriteString("Rhg")
 })
